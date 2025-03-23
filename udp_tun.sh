30a12af1d3470a4abad9f34ab3505b3a91814a63
Azumi <azumitan67@gmail.com>
2025-03-22 18:16:51 -0700

    Update udp_tun.sh

--- udp_tun.sh
+++ udp_tun.sh
@@ -4,7 +4,7 @@ apt install wget -y
 wget -O /etc/logo2.sh https://github.com/Azumi67/UDP2RAW_FEC/raw/main/logo2.sh
 chmod +x /etc/logo2.sh
 if [ -f "udp_tun.py" ]; then
-    rm wangyu.py
+    rm udp_tun.py
 fi
 wget https://github.com/Azumi67/udp_tun/releases/download/v1.0/udp_tun.py
 python3 udp_tun.py
